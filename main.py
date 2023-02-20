c15e0ff31441a6913121e3cddabfba241e48bc9a
s2448741 <s2448741@ed.ac.uk>
2023-02-20 00:44:00 +0000

    First approximation to API data extraction

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import requests
 
-api_source = "https://environment.data.gov.uk/flood-monitoring/id/stations/1491TH/readings?since=2023-02-17T23:23:00Z"
+api_source = "https://environment.data.gov.uk/flood-monitoring/id/stations/"
+station = "1491TH"
+date = "2023-02-17T23:23:00Z"
+readings_request = "/readings?since="+date
 
-content_response = requests.get(api_source)
+full_source = api_source+station+readings_request
+
+content_response = requests.get(full_source)
 
 response_status = content_response.status_code
 response_json = content_response.json()
 
 print(response_status)
-print(response_json)
+
+all_measurements = {}
+
+for item in response_json['items']:
+    measure = item['measure'].split('/')[-1]
+    value = item['value']
+    if measure not in all_measurements.keys():
+        all_measurements[measure] = [value] 
+        print('New measure '+measure+' '+str(value))
+    else:
+        print(value)
+    
